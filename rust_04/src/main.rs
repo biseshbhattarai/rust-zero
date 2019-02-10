2b8d050ec584aadcfca2cfeb6e9612971876a4c0
biseshbhattarai <aashishbhattarai49@gmail.com>
2019-02-10 20:04:42 +0545

    project 4 data types

--- rust_04/src/main.rs
+++ rust_04/src/main.rs
@@ -10,4 +10,58 @@ fn main() {
     let y = "  ";
     let y = y.len(); // This code runs
     println!("{}", y);
+
+
+    // Part 2
+    // Data types
+
+
+	// Length	Signed	Unsigned
+	// 8-bit	i8	      u8
+	// 16-bit	i16		  u16
+	// 32-bit	i32		  u32
+	// 64-bit	i64		  u64
+	// 128-bit	i128	  u128
+	// arch		isize	  usize
+
+
+	    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+
+    // remainder
+    let remainder = 43 % 5;
+
+    //Boolean Type
+
+    let t = true;
+
+    let f: bool = false; // with explicit type annotation
+
+    //Chars type
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    //Tuple type
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    //Array type
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
+}
 }
